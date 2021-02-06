86af40bd54c75aea579ccfcb0d1c9edd6ef92609
LipzDev <filipe.gt1998@outlook.com>
2021-02-06 18:04:01 -0300

    Arquivos

---
+++ js/modules/menu_desk_selected.js
@@ -0,0 +1,26 @@
+export default function initSelectedMenu(){
+   let menu = document.querySelectorAll('.menu li');
+
+   menu.forEach((item) => {
+      item.addEventListener('click', function(){
+
+         let actived = document.querySelector('.actived');
+
+         if(actived.classList.contains('actived')){
+            actived.classList.remove('actived');
+         }
+
+         item.classList.add('actived');
+
+      });
+   });
+
+
+
+
+
+
+
+
+
+}
