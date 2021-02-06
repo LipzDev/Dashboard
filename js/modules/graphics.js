1c34fdd941f8e9ff9de2624d1f44468221e30b90
LipzDev <filipe.gt1998@outlook.com>
2021-02-06 19:13:41 -0300

    Arquivos

--- js/modules/graphics.js
+++ js/modules/graphics.js
@@ -23,25 +23,6 @@ var myChart = new Chart(ctx, {
     },   
 });
 
-// GRAFICO DE LINHA
-
-var ctx = document.getElementById('myCharLine').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ['Blue','Pink','Yellow'],
-        datasets: [{   
-         label: 'Line',         
-            data: [5,3,2],            
-            borderColor: [
-               'rgba(54, 162, 235, 1)',
-               'rgba(255, 99, 132, 1)',
-               'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 3
-        }]
-    },   
-});
 
 // GRAFICO DE BARRAS
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// import initGraphics from './modules/graphics.js';
+import initGraphics from './modules/graphics.js';
 import initSelectedMenu from './modules/menu_desk_selected.js';
 
-// initGraphics();
+initGraphics();
 initSelectedMenu();
 
 
