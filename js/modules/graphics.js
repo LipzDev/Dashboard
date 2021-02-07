290e723f12346b5442df23b7f70a878bebff9294
LipzDev <filipe.gt1998@outlook.com>
2021-02-07 14:01:27 -0300

    Arquivos

--- js/modules/graphics.js
+++ js/modules/graphics.js
@@ -49,3 +49,106 @@ var myChart = new Chart(ctx, {
    },  
 });
 }
+
+// GRAFICO DE LINE
+
+var ctx = document.getElementById('myChartLine').getContext('2d');
+var myChart = new Chart(ctx, {
+   type: 'line',
+   data: {
+       labels: ['Blue', 'Pink', 'Yellow'],
+       datasets: [{
+           label: 'Graphic',
+           data: [12,1,7.5],
+           backgroundColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+           borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'            
+           ],
+           borderWidth: 1
+       }]
+   },  
+});
+
+// GRAFICO DE LINE
+
+var ctx = document.getElementById('myChartLine2').getContext('2d');
+var myChart = new Chart(ctx, {
+   type: 'line',
+   data: {
+       labels: ['Blue', 'Pink', 'Yellow'],
+       datasets: [{
+           label: 'Graphic',
+           data: [12,1,7.5],
+           backgroundColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+           borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'            
+           ],
+           borderWidth: 1
+       }]
+   },  
+});
+
+// GRAFICO DE LINE
+
+var ctx = document.getElementById('myChartLine3').getContext('2d');
+var myChart = new Chart(ctx, {
+   type: 'line',
+   data: {
+       labels: ['Blue', 'Pink', 'Yellow'],
+       datasets: [{
+           label: 'Graphic',
+           data: [12,1,7.5],
+           backgroundColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+           borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'            
+           ],
+           borderWidth: 1
+       }]
+   },  
+});
+
+// GRAFICO DE LINE
+
+var ctx = document.getElementById('myChartLine4').getContext('2d');
+var myChart = new Chart(ctx, {
+   type: 'line',
+   data: {
+       labels: ['Blue', 'Pink', 'Yellow'],
+       datasets: [{
+           label: 'Graphic',
+           data: [12,1,7.5],
+           backgroundColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'
+        ],
+           borderColor: [
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 99, 132, 1)',
+            'rgba(255, 206, 86, 1)'            
+           ],
+           borderWidth: 1
+       }]
+   },  
+});
+
+
+
